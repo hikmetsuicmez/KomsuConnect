c2cb8fecc792b3ff443e8d3d552022ac46a1b5ea
Hikmet <hikmet.suicmez1@gmail.com>
2024-10-29 01:22:36 +0300

    set up initial Spring Boot project structure

---
+++ src/test/java/com/hikmetsuicmez/komsu_connect/KomsuConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikmetsuicmez.komsu_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomsuConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
